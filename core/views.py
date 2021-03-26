362a994e419091dc6fecf68f9307ca4ca24afba7
rgrubba <rgrubba@gmail.com>
2021-03-26 07:29:19 +0000

    improved layout, added analytics

--- core/views.py
+++ core/views.py
@@ -12,7 +12,8 @@ def inject_now():
 
 @core_bp.route('/')
 def index():
-        return render_template('core/index.html')
+    days = Reservation.query.group_by(extract('year',Reservation.start), extract('month',Reservation.start), extract('day',Reservation.start)).all()
+    return render_template('core/index.html',days=days)
 
 
 @core_bp.route('/echo/')
@@ -25,16 +26,21 @@ def html():
     r = json.loads(requests.get('https://airspace.pansa.pl/map-configuration/uup').text)
     return render_template('core/uup.html',uup=r)
 
+@core_bp.route('/today/')
+def today():
+    today = datetime.date.today()
+    return day_display(today.year,today.month,today.day)
+
 @core_bp.route('/<int:year>/<int:month>/<int:day>/')
 def day_display(year,month,day):
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    yesterday_reservations = Reservation.query.filter(extract('year',Reservation.start)==yesterday.year,extract('month',Reservation.start)==yesterday.month, extract('day',Reservation.start)==yesterday.day, extract('day',Reservation.end)==day).all()
-    reservations = Reservation.query.filter(extract('year',Reservation.start)==year,extract('month',Reservation.start)==month, extract('day',Reservation.start)==day,Reservation.status.has(Status.name=="ACTIVATED")).all()
+    previous_reservations = Reservation.query.filter(extract('year',Reservation.start)==yesterday.year,extract('month',Reservation.start)==yesterday.month, extract('day',Reservation.start)==yesterday.day, extract('day',Reservation.end)==datetime.date(year=year,month=month,day=day).day).all()
+    active = Reservation.query.filter(extract('year',Reservation.start)==year,extract('month',Reservation.start)==month, extract('day',Reservation.start)==day,Reservation.status.has(Status.name=="ACTIVATED")).all()
     other= Reservation.query.filter(extract('year',Reservation.start)==year,extract('month',Reservation.start)==month, extract('day',Reservation.start)==day, Reservation.status.has(Status.name!="ACTIVATED")).all()
-    if (reservations or other):
-        return render_template('core/reservations.html',reservations=reservations, other=other, yesterday=yesterday_reservations)
+    if (yesterday or active or other):
+        return render_template('core/reservations.html',reservations=active, other=other, yesterday=previous_reservations,year=year,month=month,day=day)
     else:
-        return render_template('index.html')
+        return index()
 
 
 @core_bp.route('/update/')
